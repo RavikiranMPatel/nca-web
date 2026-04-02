1468c1b88f449948143031489439208768e63ba9
Ravikiran M Patel <rkmpatel8@gmail.com>
2026-04-02 22:48:04 +0530

    updated code

--- public/sw.js
+++ public/sw.js
@@ -1,59 +1,9 @@
-const CACHE_NAME = "nca-mysuru-v3"; // ← bump again to force replacement
-
-const STATIC_ASSETS = [
-  "/",
-  "/index.html",
-  "/favicon.ico",
-  "/favicon-96x96.png",
-  "/apple-touch-icon.png",
-  "/og-image.png",
-];
-
-// Install
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS)),
-  );
-  self.skipWaiting();
-});
-
-// Activate
+self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches
       .keys()
-      .then((keys) =>
-        Promise.all(
-          keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)),
-        ),
-      )
+      .then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
       .then(() => self.clients.claim()),
-  );
-});
-
-// Fetch
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-
-  const url = new URL(event.request.url);
-  if (
-    url.pathname.startsWith("/api/") ||
-    url.pathname.startsWith("/uploads/")
-  ) {
-    return;
-  }
-
-  event.respondWith(
-    fetch(event.request)
-      .then((res) => {
-        if (res.ok) {
-          const clone = res.clone();
-          caches
-            .open(CACHE_NAME)
-            .then((cache) => cache.put(event.request, clone));
-        }
-        return res;
-      })
-      .catch(() => caches.match(event.request)),
   );
 });
